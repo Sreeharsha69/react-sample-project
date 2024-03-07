87d2115b3ceb709cee6e31c935106a9794911061
Sreeharsha69 <144937258+Sreeharsha69@users.noreply.github.com>
2024-03-07 09:14:27 +0530

    Add files via upload

---
+++ ProjectTuneHub-1Sample/src/main/java/com/example/tunehub/ProjectTuneHub1SampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.tunehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectTuneHub1SampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectTuneHub1SampleApplication.class, args);
+	}
+
+}
